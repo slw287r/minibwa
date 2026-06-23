slw287r/minibwa

commit f9cfd78307014e7bb295b6340dd2b497a2096318

Fix seed dedup: rev only the current x[0] group (#39)
Previous rev on whole array scrambles the sort, breaking dedup.

seed.c
@@ -117,7 +117,7 @@ static void mb_seed_sort_dedup(mb_sai_v *u)
 			if (i - i0 > 1) {
 				int64_t k, k0;
 				radix_sort_mb_sais(&u->a[i0], &u->a[i]); // sort by ::size
-				kom_reverse(mb_sai_t, u->n, u->a);
+				kom_reverse(mb_sai_t, (i - i0), (u->a + i0));
 				for (k = i0 + 1, k0 = i0; k <= i; ++k) {
 					if (k == i || u->a[k0].size != u->a[k].size) {
 						if (k - k0 > 1)

commit 27f2821c796f85cc624ad9c707bce2d399fac408

r383: fixed a crash caused by #39
The curse of macro!

minibwa.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define MB_VERSION "0.2-r382-dirty"
+#define MB_VERSION "0.2-r383-dirty"
 
 #define MB_F_PAF              (0x1LL)       // output in the PAF format
 #define MB_F_NO_UNMAP         (0x2LL)       // output unmapped query sequences

seed.c
@@ -115,9 +115,10 @@ static void mb_seed_sort_dedup(mb_sai_v *u)
 	for (i = 1, i0 = 0; i <= u->n; ++i) {
 		if (i == u->n || u->a[i].x[0] != u->a[i0].x[0]) {
 			if (i - i0 > 1) {
-				int64_t k, k0;
+				int64_t k, k0, n1 = i - i0;
+				mb_sai_t *a1 = &u->a[i0];
 				radix_sort_mb_sais(&u->a[i0], &u->a[i]); // sort by ::size
-				kom_reverse(mb_sai_t, (i - i0), (u->a + i0));
+				kom_reverse(mb_sai_t, n1, a1);
 				for (k = i0 + 1, k0 = i0; k <= i; ++k) {
 					if (k == i || u->a[k0].size != u->a[k].size) {
 						if (k - k0 > 1)